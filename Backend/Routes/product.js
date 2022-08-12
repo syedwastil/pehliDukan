02ca4c6e1e1827a283ae06f1e45ed5470f45ff3e
Muhammad Abdur Rehman Wasti <45505896+syedwastil@users.noreply.github.com>
2022-08-12 12:18:42 +0500

    Product routes and controller updated

--- Backend/Routes/product.js
+++ Backend/Routes/product.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { create, productById,readSingle,deleteProduct,updateProduct } = require("../controllers/product");
+const { create, 
+        productById,
+        readSingle,
+        deleteProduct,
+        updateProduct,
+        list,
+        listRelated,
+        listCategories,
+        listBySearch,
+        productPhoto,
+     } = require("../controllers/product");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const {userById}=require('../controllers/user')
 
@@ -10,5 +20,10 @@ router.delete("/product/:productId/:userId", requireSignin,isAuth, isAdmin,delet
 router.put("/product/:productId/:userId", requireSignin,isAuth, isAdmin,updateProduct)
 router.param("userId",userById)
 router.param("productId",productById)
+router.get("/products",list)
+router.get("/products/related/:productId",listRelated)
+router.get("/products/categories", listCategories)
+router.post("/products/by/search", listBySearch)
+router.post("/product/photo/:productId", productPhoto)
 
 module.exports=router
