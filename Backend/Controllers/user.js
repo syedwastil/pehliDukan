11315212862b075e3cc530679d82054a9b5258ad
Muhammad Abdur Rehman Wasti <45505896+syedwastil@users.noreply.github.com>
2022-08-12 15:17:33 +0500

    User Routes and Controller Update

--- Backend/Controllers/user.js
+++ Backend/Controllers/user.js
@@ -1,12 +1,39 @@
 const User=require('../Model/user')
+
 exports.userById=async(req,res,next,id)=>{
     User.findById(id).exec((err,user)=>{
         if(err || !user){
             res.status(400).json({error:"User not found"})
         }else{
             req.profile=user;
+           //console.log("Storing in req profile ",req.profile)
         }
         next();
     })
 
+}
+
+exports.read=(req,res)=>{
+    //console.log("getting from req profile ",req.profile)
+
+    req.profile.hashed_password=undefined;
+    req.profile.salt=undefined;
+    return res.status(200).json(req.profile);
+}
+
+exports.update=(req,res)=>{
+
+ User.findOneAndUpdate(
+    {_id:req.profile._id},
+    {$set:req.body},
+    {new:true},
+    (err,user)=>{
+    if(err){
+        res.status(400).json({error:'Cannot update user'});
+    }else{
+        user.hashed_password=undefined;
+        user.salt=undefined;
+        res.json(user);
+    }
+ })
 }
--- Backend/Routes/user.js
+++ Backend/Routes/user.js
@@ -1,15 +1,17 @@
 const express=require('express');
-const {userById}=require('../Controllers/user')
+const {userById,read,update}=require('../Controllers/user')
 const {requireSignin,isAuth,isAdmin} =require('../Controllers/auth');
 const router=express.Router();
 
 
-router.get('/secret/:userId',requireSignin,isAuth,(req,res)=>{
+router.get('/secret/:userId',requireSignin,isAuth,isAdmin,(req,res)=>{
    res.json({
       user:req.profile
    })
 
 });
+router.get("/user/:userId",requireSignin,isAuth,read);
+router.put("/user/:userId",requireSignin,isAuth,update);
 router.param("userId",userById);
 
 module.exports=router;requireSignin
